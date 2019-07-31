89b05c8d7f3d64c1924b7cfb6d1ab19df4707b1b
thesidbatra <thesidbatra@gmail.com>
2019-07-31 12:08:26 -0400

    restructuring AGAIN

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 -- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS todolist;
+DROP DATABASE IF EXISTS trucklist;
 -- Creates the "todolist" database --
-CREATE DATABASE todolist;
+CREATE DATABASE trucklist;
